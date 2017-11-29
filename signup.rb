6361eb51864045b2620cc8dc0b98a8f85a74dc6f
shipp11 <shippdaddy1123@gmail.com>
2017-11-29 18:26:44 -0500

    signup2

--- signup.rb
+++ signup.rb
@@ -1,4 +1,5 @@
 def set_up_username(username)
+	username = username.to_s
 	username.class
 
 end
--- signup_test.rb
+++ signup_test.rb
@@ -2,15 +2,18 @@ require "minitest/autorun"
 require_relative "Signup.rb"
 class TestSignup < Minitest::Test
 
-
 	def test_assert_that_1_equals_1
 		assert_equal(1, 1)
 	end
 
 	def test_assert_that_username_are_strings
-		username = jeremy
-		assert_equal(String, set_up_username))
-
-
+		username = "jeremy"
+		assert_equal(String, set_up_username(username))
 	end
+
+	def test_assert_that_intergers_are_still_strings
+		username = 1254
+		assert_equal(String, set_up_username(username))
+	end
+
 end
