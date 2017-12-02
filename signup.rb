b0ea8ba7bed7a6c73218281d64c82a72c6a0859b
shipp11 <shippdaddy1123@gmail.com>
2017-12-02 09:56:54 -0500

    email7

--- signup.rb
+++ signup.rb
@@ -29,14 +29,54 @@ def set_up_email2(email2, email)
 		end
 	end
 
-def set_up_password(password)
+def set_up_password_length(password)
+	cap = [*('A'..'Z')]
+	lower = [*('a'..'z')]
+	special = ["!", "@", "#", "$", "%", "^", "&", "*", ":", "."]
+	numbers = [*('0'..'9')] 
 	password = password.to_s
-	if password.length > 8 
-		"correct password"
+	if password.length > 7 and
+		"valid"
 	else
 		"invalid"
 	end
 end
+
+def set_up_password_cap(password)
+	if set_up_password_length(password) == "valid"
+		set_up_password_length(password)
+		character = password.split('')
+		character.each do |letter|
+			if cap.include?(letter) == true
+				has_cap = "valid"
+			end
+			if lower.include?(letter) == true
+				has_lower = "valid"
+			end
+			if special.include?(letter) == true
+				has_special = "valid"
+			end
+			if number.include?(letter) == true
+				has_number = "valid"
+			end
+		end
+	end
+	if has_cap = "valid" and
+		has_lower = "valid" and
+		has_special = "valid" and
+		has_number = "valid" 
+		"very strong"
+	else
+		"too weak"
+	end
+
+end
+
+
+
+
+
+
 
 def set_up_password2(password2, password)
 	password2 = password2.to_s
